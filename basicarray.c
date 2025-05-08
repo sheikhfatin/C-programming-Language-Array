c49dd54ebc70af36ab16eb3f6666c81c858e53ac
Sheikh Fatin <sheikhfatin24@gmail.com>
2025-05-08 18:02:54 +0600

    Add files via upload

---
+++ basicarray.c
@@ -0,0 +1,17 @@
+ #include<stdio.h>
+ int main(){
+    //  int n[5]={2,4,6,8,1};
+    //  for(int i=0; i<=4; i++){
+    //     printf("%d\n", n[i]);
+    //  }
+    int arr[5];
+    for(int i=0; i<=4; i++){
+        int a=i+1;
+        printf("enter element number %d : ", i);
+        scanf("%d", &arr[i]);
+    
+        
+    }printf("%d", arr[2]);
+     
+     return 0;
+ }
